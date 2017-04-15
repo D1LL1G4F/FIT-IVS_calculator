ed84d04a5a0e5c60e55fb1200b955a310d832d98
Matej Kňazík <D1LL1G4F@users.noreply.github.com>
2017-04-15 15:29:41 +0200

    Math libraries
    TODO all...

---
+++ src/mathlib.cpp
@@ -0,0 +1,47 @@
+#include "mathlib.h"
+
+
+double sum(double a, double b)
+{
+    //TODO...
+}
+
+double sub(double a, double b)
+{
+    //TODO...
+}
+
+double mul(double a, double b)
+{
+    //TODO...
+}
+
+double div(double divident, double divisor)
+{
+    //TODO...
+}
+
+double sqroot(double radicand)
+{
+    //TODO...
+}
+
+double fact(double num)
+{
+    //TODO...
+}
+
+double pwr(double variable, double constant)
+{
+    //TODO...
+}
+
+double cosx(double x)
+{
+    //TODO...
+}
+
+double sinx(double x)
+{
+    //TODO...
+}
---
+++ src/mathlib.h
@@ -0,0 +1,26 @@
+#ifndef MATHLIB_H
+#define MATHLIB_H
+
+double sum(double a, double b);
+
+double sub(double a, double b);
+
+double mul(double a, double b);
+
+double div(double divident, double divisor);
+
+double sqroot(double radicand);
+
+double fact(double num);
+
+double pwr(double variable, double constant);
+
+double cosx(double x);
+
+double sinx(double x);
+
+
+
+
+
+#endif // MATHLIB_H
