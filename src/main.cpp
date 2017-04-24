9220681f98a7af652bc15b95e5de569fb2d57671
Matej Kňazík <D1LL1G4F@users.noreply.github.com>
2017-04-24 11:53:19 +0200

    main.cpp documented

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,22 @@
+/**
+  * @file main.c
+  *
+  * @author Bakaľárová Alica
+  * @author Jedlička Jakub
+  * @author Kňazík Matej
+  * @author Lovíšek Vladimír
+  * @date 24 Apr 2017
+  *
+  * @brief Calculator application made for school project in IVS subject on FIT VUT
+  *
+  * This app is made in Qt quick controls 2. This main.cpp file only runs engine and loads GUI
+  * main.qml file where all the magic happens. Also registers calcinterface module as import for
+  * all qml files.
+  */
+
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QIcon>
 
 #include "calcinterface.h"
 
@@ -7,6 +24,8 @@ int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QGuiApplication app(argc, argv);
+
+
 
 
     qmlRegisterType<CalcInterface>("CalcInterface",1,0,"CalcInterface"); // Creates registered type for QML import
